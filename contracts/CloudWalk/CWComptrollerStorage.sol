b50791c3ab3f1b49fd40d101202ec8786047f158
Igor Senych <42384549+gitigs@users.noreply.github.com>
2021-03-18 12:32:47 +0200

    ComptrollerStorage => CWComptrollerStorage (COMP token removed).

--- contracts/CloudWalk/CWComptrollerStorage.sol
+++ contracts/CloudWalk/CWComptrollerStorage.sol
@@ -1,60 +1,9 @@
 pragma solidity ^0.5.16;
 
 import "../CToken.sol";
-import "../PriceOracle.sol";
+import "../ComptrollerStorage.sol";
 
-contract UnitrollerAdminStorage {
-    /**
-    * @notice Administrator for this contract
-    */
-    address public admin;
-
-    /**
-    * @notice Pending administrator for this contract
-    */
-    address public pendingAdmin;
-
-    /**
-    * @notice Active brains of Unitroller
-    */
-    address public comptrollerImplementation;
-
-    /**
-    * @notice Pending brains of Unitroller
-    */
-    address public pendingComptrollerImplementation;
-}
-
-contract ComptrollerV1Storage is UnitrollerAdminStorage {
-
-    /**
-     * @notice Oracle which gives the price of any given asset
-     */
-    PriceOracle public oracle;
-
-    /**
-     * @notice Multiplier used to calculate the maximum repayAmount when liquidating a borrow
-     */
-    uint public closeFactorMantissa;
-
-    /**
-     * @notice Multiplier representing the discount on collateral that a liquidator receives
-     */
-    uint public liquidationIncentiveMantissa;
-
-    /**
-     * @notice Max number of assets a single account can participate in (borrow or use as collateral)
-     */
-    uint public maxAssets;
-
-    /**
-     * @notice Per-account mapping of "assets you are in", capped by maxAssets
-     */
-    mapping(address => CToken[]) public accountAssets;
-
-}
-
-contract ComptrollerV2Storage is ComptrollerV1Storage {
+contract CWComptrollerV2Storage is ComptrollerV1Storage {
     struct Market {
         /// @notice Whether or not this market is listed
         bool isListed;
@@ -68,9 +17,6 @@ contract ComptrollerV2Storage is ComptrollerV1Storage {
 
         /// @notice Per-market mapping of "accounts in this asset"
         mapping(address => bool) accountMembership;
-
-        /// @notice Whether or not this market receives COMP
-        bool isComped;
     }
 
     /**
@@ -94,52 +40,15 @@ contract ComptrollerV2Storage is ComptrollerV1Storage {
     mapping(address => bool) public borrowGuardianPaused;
 }
 
-contract ComptrollerV3Storage is ComptrollerV2Storage {
-    struct CompMarketState {
-        /// @notice The market's last updated compBorrowIndex or compSupplyIndex
-        uint224 index;
-
-        /// @notice The block number the index was last updated at
-        uint32 block;
-    }
-
+contract CWComptrollerV3Storage is CWComptrollerV2Storage {
     /// @notice A list of all markets
     CToken[] public allMarkets;
-
-    /// @notice The rate at which the flywheel distributes COMP, per block
-    uint public compRate;
-
-    /// @notice The portion of compRate that each market currently receives
-    mapping(address => uint) public compSpeeds;
-
-    /// @notice The COMP market supply state for each market
-    mapping(address => CompMarketState) public compSupplyState;
-
-    /// @notice The COMP market borrow state for each market
-    mapping(address => CompMarketState) public compBorrowState;
-
-    /// @notice The COMP borrow index for each market for each supplier as of the last time they accrued COMP
-    mapping(address => mapping(address => uint)) public compSupplierIndex;
-
-    /// @notice The COMP borrow index for each market for each borrower as of the last time they accrued COMP
-    mapping(address => mapping(address => uint)) public compBorrowerIndex;
-
-    /// @notice The COMP accrued but not yet transferred to each user
-    mapping(address => uint) public compAccrued;
 }
 
-contract ComptrollerV4Storage is ComptrollerV3Storage {
+contract CWComptrollerV4Storage is CWComptrollerV3Storage {
     // @notice The borrowCapGuardian can set borrowCaps to any number for any market. Lowering the borrow cap could disable borrowing on the given market.
     address public borrowCapGuardian;
 
     // @notice Borrow caps enforced by borrowAllowed for each cToken address. Defaults to zero which corresponds to unlimited borrowing.
     mapping(address => uint) public borrowCaps;
-}
-
-contract ComptrollerV5Storage is ComptrollerV4Storage {
-    /// @notice The portion of COMP that each contributor receives per block
-    mapping(address => uint) public compContributorSpeeds;
-
-    /// @notice Last block at which a contributor's COMP rewards have been allocated
-    mapping(address => uint) public lastContributorBlock;
 }
